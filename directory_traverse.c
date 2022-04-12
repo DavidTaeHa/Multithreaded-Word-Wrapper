5d854c32eb77192d6d0d1bccb108256bf64a1fd0
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2022-04-12 14:33:52 -0400

    update

--- directory_traverse.c
+++ directory_traverse.c
@@ -9,35 +9,42 @@
 #include <ctype.h>
 #include <time.h>
 
-void navDir(char *a, int b){
-    printf("%d: Navigate this folder: %s\n", b-1, a);
+void navDir(char *a, int b)
+{
+    printf("%d: Navigate this folder: %s\n", b - 1, a);
     struct dirent *de;
     DIR *dr = opendir(".");
 
-    if(dr == NULL){
+    if (dr == NULL)
+    {
         printf("%d: Empty directory: %s\n", b, a);
         return;
     }
-    while ((de = readdir(dr)) != NULL){
+    while ((de = readdir(dr)) != NULL)
+    {
         struct stat temp;
-        if(stat(de->d_name, &temp) != -1 && de->d_name[0] != '.'){
-            if(S_ISREG(temp.st_mode)){
+        if (stat(de->d_name, &temp) != -1 && de->d_name[0] != '.')
+        {
+            if (S_ISREG(temp.st_mode))
+            {
                 printf("%d: Wrap this file: %s : %s\n", b, a, de->d_name);
-                //add file path to file work queue
             }
-            else if(S_ISDIR(temp.st_mode)){
+            else if (S_ISDIR(temp.st_mode))
+            {
                 char cwd[100];
-                //char *cwd = calloc(100, sizeof(char));
-                if(getcwd(cwd, sizeof(cwd)) != NULL){
+                // char *cwd = calloc(100, sizeof(char));
+                if (getcwd(cwd, sizeof(cwd)) != NULL)
+                {
                     printf("Currently working dir: %s\n", cwd);
                     chdir(de->d_name);
-                    if(getcwd(cwd, sizeof(cwd)) != NULL){
+                    if (getcwd(cwd, sizeof(cwd)) != NULL)
+                    {
                         printf("Moving to dir: %s\n", cwd);
                         navDir(de->d_name, b++);
                     }
                     chdir("..");
                 }
-                //free(cwd);
+                // free(cwd);
             }
         }
     }
@@ -45,37 +52,45 @@ void navDir(char *a, int b){
     return;
 }
 
-int main(void){
+int main(void)
+{
     struct dirent *de;
     DIR *dr = opendir(".");
-  
-    if (dr == NULL){
-        printf("Null directory" );
+
+    if (dr == NULL)
+    {
+        printf("Null directory");
         return 0;
     }
-    while ((de = readdir(dr)) != NULL){
+    while ((de = readdir(dr)) != NULL)
+    {
         struct stat temp;
-        if(stat(de->d_name, &temp) != -1 && de->d_name[0] != '.'){
-            if(S_ISREG(temp.st_mode)){
+        if (stat(de->d_name, &temp) != -1 && de->d_name[0] != '.')
+        {
+            if (S_ISREG(temp.st_mode))
+            {
                 printf("Wrap this file: %s\n", de->d_name);
             }
-            else if(S_ISDIR(temp.st_mode)){
+            else if (S_ISDIR(temp.st_mode))
+            {
                 char cwd[100];
-                //char *cwd = calloc(100, sizeof(char));
-                if(getcwd(cwd, sizeof(cwd)) != NULL){
+                // char *cwd = calloc(100, sizeof(char));
+                if (getcwd(cwd, sizeof(cwd)) != NULL)
+                {
                     printf("Currently working dir: %s\n", cwd);
                     chdir(de->d_name);
-                    if(getcwd(cwd, sizeof(cwd)) != NULL){
+                    if (getcwd(cwd, sizeof(cwd)) != NULL)
+                    {
                         printf("Moving to dir: %s\n", cwd);
                         navDir(de->d_name, 1);
                     }
                     chdir("..");
                 }
-                //free(cwd);
+                // free(cwd);
             }
         }
     }
 
-    closedir(dr);    
+    closedir(dr);
     return 0;
 }
