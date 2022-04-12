5c7b8bf14252160694a40d7fc1dcb5667b19c286
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2022-04-12 19:08:14 -0400

    update

--- directory_traverse.c
+++ directory_traverse.c
@@ -9,11 +9,10 @@
 #include <ctype.h>
 #include <time.h>
 
-void navDir_v1(char *a, int b, char *filepath)
+void navDir(char *a, int b)
 {
     struct dirent *de;
-    chdir(a);
-    DIR *dr = opendir(".");
+    DIR *dr = opendir(a);
 
     if (dr == NULL)
     {
@@ -23,46 +22,9 @@ void navDir_v1(char *a, int b, char *filepath)
     while ((de = readdir(dr)) != NULL)
     {
         struct stat temp;
-        if (stat(de->d_name, &temp) != -1 && de->d_name[0] != '.')
-        {
-            if (S_ISDIR(temp.st_mode))
-            {
-                // Create filepath
-                char *path = filepath;
-                char *file = de->d_name;
-                int plen = strlen(path);
-                int flen = strlen(file);
-                char *newpath = malloc(plen + flen + 2);
-                memcpy(newpath, path, plen);
-                newpath[plen] = '/';
-                memcpy(newpath + plen + 1, file, flen + 1);
-                printf("%s\n", newpath);
 
-                // Navigate to next folder
-                navDir_v1(de->d_name, b, newpath);
-                chdir("..");
-            }
-        }
-    }
-    closedir(dr);
-    return;
-}
-
-void navDir_v2(char *a, int b, char *filepath)
-{
-    struct dirent *de;
-    DIR *dr = opendir(filepath);
-
-    if (dr == NULL)
-    {
-        printf("%d: Empty directory: %s\n", b, a);
-        return;
-    }
-    while ((de = readdir(dr)) != NULL)
-    {
-        struct stat temp;
         // Create filepath
-        char *path = filepath;
+        char *path = a;
         char *file = de->d_name;
         int plen = strlen(path);
         int flen = strlen(file);
@@ -70,15 +32,18 @@ void navDir_v2(char *a, int b, char *filepath)
         memcpy(newpath, path, plen);
         newpath[plen] = '/';
         memcpy(newpath + plen + 1, file, flen + 1);
-        printf("%s\n", newpath);
+
         if (stat(newpath, &temp) != -1 && de->d_name[0] != '.')
         {
-            if (S_ISDIR(temp.st_mode))
-            {
-
-                // Navigate to next folder
+            if(S_ISREG(temp.st_mode) && strstr(de->d_name, "wrap.") != de->d_name && strstr(de->d_name, ".txt")){
                 printf("%s\n", newpath);
-                navDir_v2(de->d_name, b, newpath);
+                //NOTE: add code to add the text file path to the queue
+            }
+            else if (S_ISDIR(temp.st_mode))
+            {
+                //NOTE: add code to add the directory file path to the queue
+                printf("%s\n", newpath);
+                navDir(newpath, b);
             }
         }
     }
@@ -88,27 +53,6 @@ void navDir_v2(char *a, int b, char *filepath)
 
 int main(void)
 {
-    // navDir_v1("test_folder", 1, "./test_folder");
-    navDir_v2("test_folder", 1, "./test_folder");
-    /*
-    struct dirent *de;
-    DIR *dr = opendir("test_folder");
-
-    while ((de = readdir(dr)) != NULL)
-    {
-        struct stat temp;
-        if (stat("./test_folder/next_folder", &temp) != -1 && de->d_name[0] != '.')
-        {
-            if (S_ISREG(temp.st_mode))
-            {
-                printf("File: %s\n", de->d_name);
-            }
-            else if (S_ISDIR(temp.st_mode))
-            {
-                printf("Directory: %s\n", de->d_name);
-            }
-        }
-    }
-    */
+    navDir("./test_folder", 1);
     return 0;
 }
