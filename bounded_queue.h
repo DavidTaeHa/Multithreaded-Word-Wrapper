775e68be7025bdaa01948c047e77f4df29802a47
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2022-04-16 23:01:30 -0400

    update

--- bounded_queue.h
+++ bounded_queue.h
@@ -9,22 +9,20 @@
 #endif
 
 #define FILEPATH 4096
-#define QUEUESIZE 16
+#define MAXSIZE 16
 
 struct bounded_queue
 {
     char **names;
     int start, stop;
     int full;
-    int empty;
-    int wait_status;
-    int capacity;
+    int dir_finished;
     pthread_mutex_t lock;
     pthread_cond_t enqueue_ready, dequeue_ready;
 };
 
 int bound_init(struct bounded_queue *q);
 int bound_destroy(struct bounded_queue *q);
-int bound_enqueue(char* n, struct bounded_queue *q);
-int bound_dequeue(char** n, struct bounded_queue *q);
+int bound_enqueue(char *n, struct bounded_queue *q);
+int bound_dequeue(char **n, struct bounded_queue *q);
 void bound_print(struct bounded_queue *q);
--- unbounded_queue.h
+++ unbounded_queue.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <limits.h>
@@ -9,20 +10,22 @@
 #endif
 
 #define FILEPATH 4096
-#define MAXSIZE 16
 
-struct bounded_queue
+// struct defines queue of different names
+struct unbounded_queue
 {
     char **names;
     int start, stop;
-    int full;
+    int isEmpty;
     int dir_finished;
+    int total_waiting;
+    int thread_count;
     pthread_mutex_t lock;
-    pthread_cond_t enqueue_ready, dequeue_ready;
+    pthread_cond_t dequeue_ready;
 };
 
-int bound_init(struct bounded_queue *q);
-int bound_destroy(struct bounded_queue *q);
-int bound_enqueue(char *n, struct bounded_queue *q);
-int bound_dequeue(char **n, struct bounded_queue *q);
-void bound_print(struct bounded_queue *q);
+int unbound_init(struct unbounded_queue *q, int count);
+int unbound_destroy(struct unbounded_queue *q);
+int unbound_enqueue(char *n, struct unbounded_queue *q);
+int unbound_dequeue(char **n, struct unbounded_queue *q);
+void unbound_print(struct unbounded_queue *q);
